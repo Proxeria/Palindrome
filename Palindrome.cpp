495109dbb772802baa7e40441fe9a4e0d30173cf
Proxeria <napinpdx@me.com>
2021-10-01 07:55:55 -0700

    first commit

---
+++ Palindrome.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <atlstr.h>
+/*
+Nick Peterson
+9/29/2021
+Palindrome
+This program will read in a string from the user and print 
+out if it's a palindrome or not.
+ */
+using namespace std;
+
+int main() {
+  
+  return 0;
+}
